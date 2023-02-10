dff9c02ea150f973b41afe016ed166d0746539ba
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-10 14:15:27 -0800

    add onAddItem and handleFormSub functionality

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,15 +1,37 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
-const Add = () => {
+const Add = ({ onAddClick }) => {
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+
+    const newItem = {
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: parseInt(event.target.price.value),
+      roast: event.target.roast.value,
+      unitsInStock: parseInt(event.target.units.value),
+      poundsInStock: (parseInt(event.target.units.value) * 130),
+      id: v4()
+    }
+    onAddClick(newItem);
+  }
+
   return(
-    <form>
+    <form onSubmit={handleFormSubmission}>
       <h3>Add</h3>
       <input type="text" placeholder="Name" name="name"/>
       <input type="text" placeholder="Origin" name="origin"/>
       <input type="number" placeholder="Price" name="price"/>
       <input type="text" placeholder="Roast" name="roast"/>
+      <input type="number" placeholder="Units in Stock" name="units"/>
       <button type="submit">Add</button>
     </form>
   );
+}
+
+Add.propTypes = {
+  onAddClick: PropTypes.func
 }
 export default Add;
