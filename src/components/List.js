943d2ecf0a5377c9e8f4d746101c66665b663004
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-10 13:54:22 -0800

    remove comments

--- src/components/List.js
+++ src/components/List.js
@@ -6,12 +6,10 @@ const List = ({ inventory, onItemClick }) => {
   return (
     <React.Fragment>
       {inventory.map((item) => (
-        // TODO: fix this, getting error InventoryControl.js:21 Uncaught TypeError: Cannot read properties of undefined (reading 'state')
-        // at handleDisplayDetail (InventoryControl.js:21:1)
         <div key={item.id} onClick={() => onItemClick(item.id)}>
           <h4>{item.name}</h4>
           <p>{item.blend}</p>
-          <p>{item.price}</p>
+          <p>${item.price}</p>
           <p>{item.origin}</p>
         </div>
       ))
