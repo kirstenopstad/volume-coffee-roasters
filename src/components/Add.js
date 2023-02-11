814435ddfe149216ff9c86f27c373e85058a5606
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-11 11:07:23 -0800

    add form control styling to Add form

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import placeholderImage from './../img/beans/placeholder.png'
+import Modal from 'react-bootstrap/Modal';
+
 const Add = ({ onAddClick }) => {
+
+  // handle form
   const handleFormSubmission = (event) => {
     event.preventDefault();
 
@@ -10,23 +17,35 @@ const Add = ({ onAddClick }) => {
       origin: event.target.origin.value,
       price: parseInt(event.target.price.value),
       roast: event.target.roast.value,
+      image: placeholderImage,
       id: v4()
     }
     onAddClick(newItem);
   }
 
   return(
-    <form onSubmit={handleFormSubmission}>
-      <input type="text" placeholder="Name" name="name"/>
-      <br/>
-      <input type="text" placeholder="Origin" name="origin"/>
-      <br/>
-      <input type="number" placeholder="Price" name="price"/>
-      <br/>
-      <input type="text" placeholder="Roast" name="roast"/>
-      <br/>
-      <button type="submit">Add</button>
-    </form>
+    <React.Fragment>
+      <h3>Add</h3>
+      <Form onSubmit={handleFormSubmission}>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control placeholder="French Roast" name="name" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Origin</Form.Label>
+          <Form.Control placeholder="Single Origin / Blend" name="origin" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Price</Form.Label>
+          <Form.Control type="price" placeholder="$17" name="price" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Roast</Form.Label>
+          <Form.Control placeholder="Dark, Medium, Light" name="roast" />
+        </Form.Group>
+        <Button type="submit">Add</Button>
+      </Form>
+    </React.Fragment>
   );
 }
 
