b9d170db8f2cfd61cad47efb1534463a71476e1d
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-28 11:10:46 -0700

    clean up code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,23 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import beans from './../img/stock/beans.jpg'
 import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from './../img/logo.png'
 
 const Header = () => {
   return(
-    <React.Fragment>
       <div className="header">
         <Navbar collapseOnSelect expand="md" variant="dark" className="navigation" >
           <Container >
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="#index">
               <img src={logo} alt="Volume Coffee Roasters logo" className="logo"/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Blog</Nav.Link>
-                <Nav.Link href="#pricing">Order</Nav.Link>
-                <Nav.Link href="#pricing">Contact</Nav.Link>
+                <Nav.Link href="#blog">Blog</Nav.Link>
+                <Nav.Link href="#Order">Order</Nav.Link>
+                <Nav.Link href="#Contact">Contact</Nav.Link>
         
               </Nav>
               <Nav>
@@ -33,11 +31,9 @@ const Header = () => {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        
-        
         <Card className="bg-dark text-white hero">
         <div className="hero-mask">
-        <Card.Img src={beans} alt="roasted coffee beans"/>
+          <Card.Img src={beans} alt="roasted coffee beans"/>
         </div>
         <Card.ImgOverlay className="hero-content">
           <Card.Title>
@@ -49,12 +45,10 @@ const Header = () => {
           <Card.Text>
             plus get 30% of your first order with code NEWBFF
           </Card.Text>
-          {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
           <Button variant="outline-light">Subscribe</Button>
         </Card.ImgOverlay>
       </Card>
-      </div>
-  </React.Fragment>
+    </div>
   );
 };
 
